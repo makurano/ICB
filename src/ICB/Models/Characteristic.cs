1509f999b5c71c1a4d119251b7f81a2cec1dc4aa
makurano <ginsyari1@gmail.com>
2022-02-10 20:40:02 +0900

    ディレクトリ復元

---
+++ src/ICB/Models/Characteristic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ICB.Models
+{
+	public class Characteristic
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public bool IsRare { get; set; }
+	}
+}
+
