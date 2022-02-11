12e5a6d35668790f1d2d48696695f3b1903ab34e
orimoto <lighthead.dev@gmail.com>
2022-02-11 23:45:58 +0900

    リポジトリのテストを追加。

---
+++ test/ICB.Tests/Repositories/CardRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using ICB.Repositories;
+using ICB.Repositories.Interfaces;
+using NUnit.Framework;
+
+namespace ICB.Tests.Repositories;
+
+public class CardRepositoryTest
+{
+    private ICardRepository _repository;
+
+    [SetUp]
+    public void Setup()
+    {
+        _repository = new DraftCardRepository();
+    }
+
+    [Test]
+    public void GetCharacteristics()
+    {
+        Assert.IsTrue(_repository.GetCharacteristics().Any());
+    }
+}
